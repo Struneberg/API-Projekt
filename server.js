c784a061b428c780eed87aab9c7f795b8cc7c2aa
Struneberg <146861637+Struneberg@users.noreply.github.com>
2023-11-14 20:09:03 +0100

    Connected to Mongo DB

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ require('dotenv-flow').config();
 
 //Route
 
+app.get("/api/welcome", (req, res) => {
+    res.status(200).send({message: "Velkommen til min API"});
+});
+mongoose.connect(
+    process.env.DBHOST,
+)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+});
+
+
+mongoose.connection.once("open", () => console.log('Connected successfully to MongoDB'));
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function(){
